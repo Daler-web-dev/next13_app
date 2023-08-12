19f764d23c66b04e85affa6dcae00d2c3089c536
Daler <dsarifkulo021@gmail.com>
2023-08-12 16:57:24 +0500

    added id page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import { prisma } from "@/lib/prisma";
+import { authOptions } from "@/utils/authOption";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import React from "react";
@@ -5,11 +7,19 @@ import React from "react";
 interface Props {}
 
 const Dashboard: React.FC<Props> = async () => {
-	const session = await getServerSession();
+	const session = await getServerSession(authOptions);
 
 	if (!session) {
 		redirect("/api/auth/signin");
 	}
+
+	const currentUserEmail = session?.user?.email!
+	const user = await prisma.user.findUnique({
+		where: {
+			email: currentUserEmail
+		}
+	})
+
 	return <div>DASHBOARD</div>;
 };
 
---
+++ app/users/[id]/page.tsx
@@ -0,0 +1,34 @@
+import { prisma } from "@/lib/prisma";
+import { Metadata } from 'next';
+import React from "react";
+
+interface Props {
+	params: {
+		id: string;
+	};
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+	const user = await prisma.user.findUnique({ where: { id: params.id } });
+	return { title: `User profile of ${user?.name}` };
+}
+
+const UserPrifile: React.FC<Props> = async ({ params }) => {
+	const user = await prisma.user.findUnique({ where: { id: params.id } });
+
+	const { name, bio, image } = user ?? {};
+
+    console.log(user);
+    
+
+	return (
+		<div>
+			<h1>{name}</h1>
+			<img src={image ?? ""} alt={`${name}'s profile`} />
+			<h3>BIO</h3>
+			<p>{bio}</p>
+		</div>
+	);
+};
+
+export default UserPrifile;
