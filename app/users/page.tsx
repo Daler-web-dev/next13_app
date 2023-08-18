846face8d5e20404fffd31812ded313d32a4f5e8
Daler <dsarifkulo021@gmail.com>
2023-08-18 15:11:05 +0500

    checking db

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,6 +4,9 @@ import { prisma } from "@/lib/prisma";
 export default async function Users() {
 	const users = await prisma.user.findMany();
 
+	console.log(users);
+	
+
 	return (
 		<div className="w-full grid grid-cols-5 gap-2" >
 			{users.map((user) => {
